aa038fb7421b2d31219e82144acd4602766128cb
CongHuyKieuVHS <conghuykieu2005@gmail.com>
2025-04-19 20:16:55 -0400

    added get/set,constructor and method to reservation,table and user

--- src/main/java/com/example/restaurantordersystem/model/User.java
+++ src/main/java/com/example/restaurantordersystem/model/User.java
@@ -1,4 +1,83 @@
 package com.example.restaurantordersystem.model;
 
+import java.util.List;
+
 public class User {
+
+    private Long userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private String role = "Customer"; // Default role, will add 2 login for customer and admin(where admin can create new item like food or even table)
+    private List<Order> orders;
+    private List<Reservation> reservations;
+
+    // Constructors
+    public User() {}
+
+    public User(String fullName, String email, String password, String role) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and Setters
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
 }
