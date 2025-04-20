0455f2e2a1d3fceae1144a7dc68cf0284a0638a3
CongHuyKieuVHS <conghuykieu2005@gmail.com>
2025-04-19 20:16:55 -0400

    added get/set,constructor and method to reservation,table and user

--- src/main/java/com/example/restaurantordersystem/model/Table.java
+++ src/main/java/com/example/restaurantordersystem/model/Table.java
@@ -1,5 +1,62 @@
 package com.example.restaurantordersystem.model;
 
+import java.util.List;
+
 public class Table {
 
+
+    private Long tableId;
+    private int tableNumber;
+    private int capacity;
+    private boolean isAvailable = true;
+    private List<Reservation> reservations; // it is a list because table can have multiple reservation and we will look if it is reserve base on the time
+    // Constructors
+    public Table() {}
+
+    public Table(int tableNumber, int capacity, boolean isAvailable) {
+        this.tableNumber = tableNumber;
+        this.capacity = capacity;
+        this.isAvailable = isAvailable;
+    }
+
+    // Getters and Setters
+    public Long getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(Long tableId) {
+        this.tableId = tableId;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
 }
