d9921da963acd467539eb5a6d7ed05091b16bbc1
CongHuyKieuVHS <conghuykieu2005@gmail.com>
2025-04-20 20:08:57 -0400

    finish paymentDao, reservationDao and tablesDao

--- src/main/java/com/example/restaurantordersystem/dao/ReservationsDAO.java
+++ src/main/java/com/example/restaurantordersystem/dao/ReservationsDAO.java
@@ -1,4 +1,15 @@
 package com.example.restaurantordersystem.dao;
 
+import com.example.restaurantordersystem.model.Reservation;
+
+import java.util.List;
+
 public interface ReservationsDAO {
+    Reservation findReservationByID(long reservationID);
+
+    List<Reservation> findAllReservations();
+
+    boolean saveReservation(Reservation reservation);
+
+    boolean deleteReservation(int id);
 }
--- src/main/java/com/example/restaurantordersystem/dao/TablesDAO.java
+++ src/main/java/com/example/restaurantordersystem/dao/TablesDAO.java
@@ -1,4 +1,14 @@
 package com.example.restaurantordersystem.dao;
+import com.example.restaurantordersystem.model.Table;
+
+import java.util.List;
 
 public interface TablesDAO {
+    Table findTableByID(long tableId);
+
+    List<Table> findAllTables();
+
+    boolean saveTable(Table table);
+
+    boolean deleteTable(int id);
 }
